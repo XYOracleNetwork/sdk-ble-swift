20996c86be21730819afc43facb238de13ca4453
Darren Sutherland <darren@xyo.network>
2018-10-02 14:16:26 -0700

    adding cache

---
+++ XyBleSdk/Classes/XYDeviceCache.swift
@@ -0,0 +1,14 @@
+//
+//  XYDeviceCache.swift
+//  Pods-XyBleSdk_Example
+//
+//  Created by Darren Sutherland on 10/2/18.
+//
+
+import CoreBluetooth
+
+class XYDeviceCache {
+
+    fileprivate static var devices = [XYFinderDevice]()
+}
+
