0434a0a08c064995df235b25e1663e7b09203676
Darren Sutherland <darren@xyo.network>
2018-10-11 10:02:36 -0700

    adding in the new event manager stuff

---
+++ XyBleSdk/Classes/Devices/Base/XYFinderDeviceEvent.swift
@@ -0,0 +1,19 @@
+//
+//  XYFinderDeviceEvent.swift
+//  Pods-XyBleSdk_Example
+//
+//  Created by Darren Sutherland on 10/11/18.
+//
+
+import Foundation
+
+public enum XYFinderEvent {
+    case connected
+    case disconnected
+    case buttonPressed // type
+    case buttonRecentlyPressed // type
+    case detected // power signal distance
+    case exiting
+    case exited
+    case updated
+}
---
+++ XyBleSdk/Classes/Devices/Base/XYFinderDeviceEventManager.swift
@@ -0,0 +1,12 @@
+//
+//  XYFinderDeviceEventManager.swift
+//  Pods-XyBleSdk_Example
+//
+//  Created by Darren Sutherland on 10/11/18.
+//
+
+import Foundation
+
+class XYFinderDeviceEventManager {
+
+}
