b32803241fc912214590a580b61f7132446cc25d
Darren Sutherland <darren@xyo.network>
2018-10-17 10:07:47 -0700

    adding unknown

--- XyBleSdk/Classes/Devices/XYFinderDeviceFamily.swift
+++ XyBleSdk/Classes/Devices/XYFinderDeviceFamily.swift
@@ -11,7 +11,8 @@ import CoreBluetooth
 
 // Defines the various XY devices supported by this SDK
 public enum XYFinderDeviceFamily: Int {
-    case xy1 = 0
+    case unknown
+    case xy1
     case xy2
     case xy3
     case xy4
@@ -38,6 +39,7 @@ public enum XYFinderDeviceFamily: Int {
         case .xymobile: return "xy:mobiledevice"
         case .xygps: return "xy:gps"
         case .xynear: return "xy:near"
+        case .unknown: return "unknown"
         }
     }
 
@@ -64,10 +66,12 @@ public enum XYFinderDeviceFamily: Int {
         case .xygps: return "XY-GPS Finder"
         case .xymobile: return "Mobile Device"
         case .xynear: return "XY-Near Finder"
+        case .unknown: return "Unknown"
         }
     }
 
     private static let uuids = [
+        "", // unknown
         "a500248c-abc2-4206-9bd7-034f4fc9ed10", // xy1
         "07775dd0-111b-11e4-9191-0800200c9a66", // xy2
         "08885dd0-111b-11e4-9191-0800200c9a66", // xy3
@@ -83,5 +87,5 @@ public enum XYFinderDeviceFamily: Int {
     fileprivate static let lockDefault = Data([0x2f, 0xbe, 0xa2, 0x07, 0x52, 0xfe, 0xbf, 0x31, 0x1d, 0xac, 0x5d, 0xfa, 0x7d, 0x77, 0x76, 0x80])
     fileprivate static let lockXy4 = Data([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f])
 
-    public static let values: [XYFinderDeviceFamily] = [xy1, xy2, xy3, xy4, xymobile, xygps, xynear]
+    public static let values: [XYFinderDeviceFamily] = [unknown, xy1, xy2, xy3, xy4, xymobile, xygps, xynear]
 }
