35193f4f34700206e7022fde95ade84285d275e0
Darren Sutherland <darren@xyo.network>
2018-11-01 09:44:16 -0700

    stub for central agent

---
+++ XyBleSdk/Classes/Devices/Agents/XYCentralAgent.swift
@@ -0,0 +1,15 @@
+//
+//  XYCentralAgent.swift
+//  Pods-XyBleSdk_Example
+//
+//  Created by Darren Sutherland on 11/1/18.
+//
+
+import Promises
+import CoreBluetooth
+
+internal final class XYCentralAgent: XYCentralDelegate {
+
+
+
+}
--- XyBleSdk/Classes/Devices/Agents/XYConnectionAgent.swift
+++ XyBleSdk/Classes/Devices/Agents/XYConnectionAgent.swift
@@ -6,6 +6,7 @@
 //
 
 import Promises
+import CoreBluetooth
 
 // A helper to allow for adding connecting to a peripheral to a connection() operation closure
 // NOTE: The agent is not thread-safe, and should be used in conjunction with other locking mechanisms
