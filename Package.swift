134609690d932ee3d4c7c45019f1c7ebf486f298
Darren Sutherland <darren@xyo.network>
2019-01-17 13:46:58 -0800

    Adding SPM hook

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "XyBleSdk",
+    dependencies: [
+        .Package(url: "https://github.com/google/promises", versions: Version(1, 2, 4)..<Version(1, 2, 5))
+    ]
+)
