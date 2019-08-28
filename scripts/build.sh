d59258ad23af3ee019b03e17a8e0a144b47544c3
Arie Trouw <arie.trouw@xy.company>
2019-08-28 14:21:28 -0700

    Build Script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,4 @@
-xcodebuild clean -workspace XyBleSdk.xcworkspace -scheme "Pods-XyBleSdk iOS" &&
 xcodebuild clean -workspace XyBleSdk.xcworkspace -scheme "XyBleSdk iOS"
 
-xcodebuild build -workspace XyBleSdk.xcworkspace -scheme "Pods-XyBleSdk iOS" &&
-xcodebuild build -workspace XyBleSdk.xcworkspace -scheme "XyBleSdk iOS" &&
-pod lib lint
+xcodebuild build -workspace XyBleSdk.xcworkspace -scheme "XyBleSdk iOS"
 
