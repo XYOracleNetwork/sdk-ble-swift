67f2bb06d4bbcd5bc0b5583bb8c8d382d37ae23e
pllearns <thenewphilliplorenzo@outlook.com>
2020-01-28 15:27:34 -0800

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
-pod trunk register arie.trouw@xyo.network 'XYO Team' --description='Deploy Script'
+pod trunk register phillip.lorenzo@xyo.network 'XYO Team' --description='github actions token'
 pod lib lint
 pod --allow-warnings trunk push XyBleSdk.podspec
