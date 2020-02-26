d5cf80ed65b3dc859088a2e964b5d3c0aa838588
pllearns <thenewphilliplorenzo@outlook.com>
2020-02-26 15:32:53 -0800

    resolve package name for swift build

--- Package.swift
+++ Package.swift
@@ -12,14 +12,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/google/promises", from: "1.2.8"),
-        .package(url: "https://github.com/XYOracleNetwork/sdk-base-swift.git", from: "1.0.2"),
+        .package(url: "https://github.com/XYOracleNetwork/sdk-base-swift.git", from: "1.0.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "sdk-ble-swift",
-            dependencies: ["Promises", "sdk-base-swift"]),
+            dependencies: ["Promises", "XyBaseSdk"]),
         .testTarget(
             name: "sdk-ble-swiftTests",
             dependencies: ["sdk-ble-swift"]),
