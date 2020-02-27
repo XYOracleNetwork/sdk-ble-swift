eeebce9c25c60ae511268bab25e8a93db8f87532
pllearns <thenewphilliplorenzo@outlook.com>
2020-02-27 14:55:40 -0800

    update config

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "XyBleSdk",
     platforms: [
-        .iOS(.v13)
+        .macOS(.v10_15),
+        .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
