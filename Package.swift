36ec69598094530c72960897c5a65fb1c9188b95
Arie Trouw <arie.trouw@xy.company>
2021-01-25 15:18:59 -0800

    Added iOS Version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
   name: "XyBleSdk",
   platforms: [
-    .macOS(.v10_13),
+    .iOS(.v8),
+    .macOS(.v10_13)
   ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
